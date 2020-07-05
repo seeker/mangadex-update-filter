345055c82a38466a9bd6e612f7dd79373b4454e0
Nicholas Wright <dozedoffagain@gmail.com>
2020-07-05 20:51:47 +0200

    Hide titles on main page

--- src/Main.ts
+++ src/Main.ts
@@ -1,5 +1,15 @@
+import { DomFilter } from "./DomFilter";
+import { Title } from "./Title";
+import { Persistence } from "./Persistence"
+
 runScript();
 
 function runScript() {
-    console.log("Script loaded!")
+    let latest = DomFilter.filterLatestTitles(document);
+    let persistence : Persistence = new Persistence("md-uf");
+
+    latest.forEach(
+        (value) => {
+            new Title(value, persistence);
+    });
 }
