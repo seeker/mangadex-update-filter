9cc78068efd6a9a03803591b687e38152985d169
Nicholas Wright <dozedoffagain@gmail.com>
2020-07-07 16:48:58 +0200

    Differ script based on URL

--- src/Main.ts
+++ src/Main.ts
@@ -1,10 +1,22 @@
 import { DomFilter } from "./DomFilter";
 import { Title } from "./Title";
 import { Persistence } from "./Persistence"
+import { Utils } from "./Utils";
 
 runScript();
 
 function runScript() {
+    if(document.URL == "https://mangadex.org/") {
+        console.log("On main page");
+        mainPage();
+    }
+
+    if(document.URL.includes("title")) {
+        console.log("On title page: " + Utils.getTitleID(document.URL));
+    }
+}
+
+function mainPage() {
     let latest = DomFilter.filterLatestTitles(document);
     let persistence : Persistence = new Persistence("md-uf-");
 
